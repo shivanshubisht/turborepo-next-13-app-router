5572ce8b7e06863696380fcac43592e3eb00ae11
shivanshu <bishtshivanshu@gmail.com>
2023-03-18 03:49:21 +0530

    configured bunch of stuff

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   reactStrictMode: true,
-  transpilePackages: ["ui"],
-};
+  transpilePackages: ['ui'],
+}
+
+module.exports = nextConfig
---
+++ apps/web/prettier.config.js
@@ -0,0 +1,4 @@
+/** @type {import("prettier").Config & { [key:string]: any }} */
+module.exports = {
+  ...require("../../prettier.config.js"),
+}
